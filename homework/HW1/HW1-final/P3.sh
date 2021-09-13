19a7bf4cbc71b7f5786b43889cdcdb6943891a26
Yiting Han <yitinghan@hsph.harvard.edu>
2021-09-13 14:35:02 -0400

    Completed Part3

--- homework/HW1/HW1-final/P3.sh
+++ homework/HW1/HW1-final/P3.sh
@@ -1,5 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 grep '[0-9]' apollo13.txt | wc -l > apollo_out.txt  # 1404
-# Part C 
+grep --help | grep "\-\-count" 
 find ./ -maxdepth 1 -type f -name "*.py" | wc -l 
-# subdirectories?
+find ./ -type f ! -perm -o=rw | wc -l # file2 file4 file5
+find ./ -maxdepth 1 ! -perm -o=rw -type f -or ! -perm -o=rw -type d | wc -l # file2 subdirecotry2
+
+
--- homework/HW1/HW1-final/P3F.sh
+++ homework/HW1/HW1-final/P3F.sh
@@ -1,1 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+for each in $(ls); do
+    if [ -f "$each" ]; then 
+            echo "$each $(cat $each | wc -l)"
+    fi
+done
