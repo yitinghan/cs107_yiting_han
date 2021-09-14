9b82ae08ff700a97db35e1f688ed819df4a4950c
Yiting Han <yitinghan@hsph.harvard.edu>
2021-09-13 21:15:30 -0400

    Updated part3

--- homework/HW1/HW1-final/P3.sh
+++ homework/HW1/HW1-final/P3.sh
@@ -3,6 +3,6 @@ grep '[0-9]' apollo13.txt | wc -l > apollo_out.txt  # 1404
 grep --help | grep "\-\-count" 
 find ./ -maxdepth 1 -type f -name "*.py" | wc -l 
 find ./ -type f ! -perm -o=rw | wc -l # file2 file4 file5
-find ./ -maxdepth 1 ! -perm -o=rw -type f -or ! -perm -o=rw -type d | wc -l # file2 subdirecotry2
+find ./ -maxdepth 1 -type d,f !  -perm -o=w -and -type d,f ! -perm -o=r | wc -l  # no read and no write file2 subdirecotry2
 
 
